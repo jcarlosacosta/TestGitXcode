fc030f6fda8273151b445c62023e0b18199e775b
JorgeAcostaFreire <jorge.acosta.freire@gmail.com>
2022-07-14 22:46:51 +0200

    Prueba añadiendo proyecto entero

---
+++ JorgeTest/JorgeTest/JorgeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JorgeTestApp.swift
+//  JorgeTest
+//
+//  Created by Jorge Acosta Freire on 14/7/22.
+//
+
+import SwiftUI
+
+@main
+struct JorgeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
